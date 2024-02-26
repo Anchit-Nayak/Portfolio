9bab37ec8852011554962902de1649c4533b2390
Anchit Nayak <anchitnayak2003@gmail.com>
2024-02-26 21:04:54 +0530

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import React, { Suspense} from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
@@ -9,58 +9,60 @@ const Hero = () => {
       <div className="flex justify-center items-center h-screen w-full">
         <div className="flex w-full flex-col gap-2 text-center justify-center text-xl h-full lg:h-full">
           <div className='absolute inset-0 z-10 flex flex-col justify-center items-center'>
-          <h1>Hi my name is,</h1>
-          <motion.div
-           variants={{
-            hidden: { opacity: 0 , y: 75 },
-            visible: { opacity: 1, y: 0 }
-           }}
-           initial="hidden"
-           animate="visible"
-           transition={{ duration: 0.7, delay: 3.2}}
-          >
-          <h1 class="tracking-tighter z-10 text-center text-5xl py-4 lg:py-4 md:py-5 sm:py-2 lg:text-9xl text-transparent duration-5000 bg-white cursor-default text-edge-outline animate-title font-inter sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text font-bold text-shadow-2xl shadow-black">Anchit Nayak.</h1>
-          </motion.div>
-          
+            <h1>Hi my name is,</h1>
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: 75 },
+                visible: { opacity: 1, y: 0 }
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.7, delay: 3.2 }}
+            >
+              <h1 class="tracking-tighter z-10 text-center text-5xl py-4 lg:py-4 md:py-5 sm:py-2 lg:text-9xl text-transparent duration-5000 bg-white cursor-default text-edge-outline animate-title font-inter sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text font-bold text-shadow-2xl shadow-black">Anchit Nayak.</h1>
+            </motion.div>
 
-          <motion.div
-          variants={{
-            hidden: { opacity: 0 , y: -75 },
-            visible: { opacity: 1, y: 0 }
-           }}
-           initial="hidden"
-           animate="visible"
-           transition={{ duration: 0.7, delay: 3.8}}
-          >
-          <div className="text-gray-100 lg:text-3xl font-bold">
-            I wield code to create wonders.
-          </div>
-          
-          <div className=" text-ellipsis text-center text-gray-400 text-sm lg:text-lg px-3 lg:px-0">
-            Driven software engineer with a passion for learning, a problem-solving mindset, and expertise in delivering innovative and efficient software products.
-          </div>
-          </motion.div>
+
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: -75 },
+                visible: { opacity: 1, y: 0 }
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.7, delay: 3.8 }}
+            >
+              <div className="text-gray-100 lg:text-3xl font-bold">
+                I wield code to create wonders.
+              </div>
+
+              <div className=" text-ellipsis text-center text-gray-400 text-sm lg:text-lg px-3 lg:px-0">
+                Driven software engineer with a passion for learning, a problem-solving mindset, and expertise in delivering innovative and efficient software products.
+              </div>
+            </motion.div>
+
           </div>
 
           <div className='inset-0 w-full h-full flex justify-center items-center'>
-          <Canvas>
-            <Suspense fallback={null}>
-              <OrbitControls enableZoom={false} />
-              <ambientLight intensity={0} />
-              <directionalLight position={[3, 2, 1]} />
-              <Sphere args={[1, 100, 200]} scale={2.0}>
-                <MeshDistortMaterial
-                  color="rgb(0, 0, 0)"
-                  attach="material"
-                  distort={0.5}
-                  speed={5}
-                />
-                
-              </Sphere>
-              
-            </Suspense>
-            
-          </Canvas>
+            <Canvas>
+              <Suspense fallback={null}>
+                <OrbitControls enableZoom={false} />
+                <ambientLight intensity={0} />
+                <directionalLight position={[3, 2, 1]} />
+                <Sphere args={[1, 100, 200]} scale={2.0}>
+                  <MeshDistortMaterial
+                    color="rgb(0, 0, 0)"
+                    attach="material"
+                    distort={0.5}
+                    speed={5}
+                  />
+
+                </Sphere>
+
+              </Suspense>
+
+            </Canvas>
+
           </div>
         </div>
       </div>
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -78,49 +78,56 @@ const Page = () => {
 
   return (
     <div className="mb-44 w-full">
-    <div className="flex justify-center items-center h-screen w-full">
-      <div className="flex w-full flex-col gap-2 text-center justify-center text-xl h-full lg:h-full">
-        <div className='relative inset-0 z-50 flex flex-col justify-center items-center mix-blend-difference'>
-        <h1>Hi my name is,</h1>
-        <motion.div
-         variants={{
-          hidden: { opacity: 0 , y: 75 },
-          visible: { opacity: 1, y: 0 }
-         }}
-         initial="hidden"
-         animate="visible"
-         transition={{ duration: 0.7, delay: 3.2}}
-        >
-        <h1 class="tracking-tighter text-center text-5xl py-4 lg:py-4 md:py-5 sm:py-2 lg:text-9xl text-transparent duration-5000 bg-white cursor-default text-edge-outline animate-title font-inter sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text font-bold text-shadow-2xl shadow-black">Anchit Nayak.</h1>
-        </motion.div>
-        
+      <div className="flex justify-center items-center h-screen w-full">
+        <div className="flex w-full flex-col gap-2 text-center justify-center text-xl h-full lg:h-full">
+          <div className='relative inset-0 z-50 flex flex-col justify-center items-center mix-blend-difference'>
+            <h1>Hi my name is,</h1>
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: 75 },
+                visible: { opacity: 1, y: 0 }
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.7, delay: 3.2 }}
+            >
+              <h1 class="tracking-tighter text-center text-5xl py-4 lg:py-4 md:py-5 sm:py-2 lg:text-9xl text-transparent duration-5000 bg-white cursor-default text-edge-outline animate-title font-inter sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text font-bold text-shadow-2xl shadow-black">Anchit Nayak.</h1>
+            </motion.div>
 
-        <motion.div
-        variants={{
-          hidden: { opacity: 0 , y: -75 },
-          visible: { opacity: 1, y: 0 }
-         }}
-         initial="hidden"
-         animate="visible"
-         transition={{ duration: 0.7, delay: 3.8}}
-        >
-        <div className="text-gray-100 lg:text-3xl font-bold">
-          I wield code to create wonders.
-        </div>
-        
-        <div className=" text-ellipsis text-center text-gray-400 text-sm lg:text-lg px-3 lg:px-0">
-          Driven software engineer with a passion for learning, a problem-solving mindset, and expertise in delivering innovative and efficient software products.
-        </div>
-        </motion.div>
-        </div>
 
-        <div className='absolute inset-0 z-20 w-full h-full flex justify-center items-center'>
-        <Scene/>
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: -75 },
+                visible: { opacity: 1, y: 0 }
+              }}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.7, delay: 3.8 }}
+            >
+              <div className="text-gray-100 lg:text-3xl font-bold">
+                I wield code to create wonders.
+              </div>
+
+              <div className=" text-ellipsis text-center text-gray-400 text-sm lg:text-lg px-3 lg:px-0">
+                Driven software engineer with a passion for learning, a problem-solving mindset, and expertise in delivering innovative and efficient software products.
+              </div>
+            </motion.div>
+            <a href="#About" class="absolute inset-0 z-30 bottom-0 flex w-full justify-center top-44 lg:top-80">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 text-white mt-52 animate-bounce">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+            </a>
+
+          </div>
+
+
+          <div className='absolute inset-0 z-20 w-full h-full flex justify-center items-center'>
+            <Scene />
+          </div>
         </div>
       </div>
     </div>
-  </div>
   )
-  
+
 }
 export default Page
